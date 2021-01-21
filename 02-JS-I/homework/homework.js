c4781432f9cdd150926bb87e07ffe1b7bee9be3d
Luis Armando <luisarmando123@hotmail.com>
2021-01-21 08:04:22 -0300

    Homework JS-I de nuevo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -35,70 +35,68 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código: 
-  return x + y;
+  return (x+y);
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código: 
-  return x - y;
+  return (x - y);
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código: 
-  return x * y;
+  return (x * y);
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código: 
-  return x / y;
+ return (x / y) ;
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código: 
-  if (x === y) {
-    return true;
-  } else {
-    return false;
-  }
-  
+ if (x === y){
+   return true;
+ } 
+ else return false
 }
 
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-if (str1. length === str2. length) {
+if (str1. length === str2. length){
   return true;
-} else {
-  return false;
 }
+else return false;
 }
+
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
 if (num < 90) {
-  return true;
-} else {
-  return false;
+ return true;
 }
+else 
+ return false
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num > 50) {
-    return true;
-  } else {
-    return false;
-  }
+if (num > 50){
+  return true;
+}
+else
+ return false
 }
 
 function obtenerResto(x, y) {
@@ -111,29 +109,29 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num % 2 ===0) {
-    return true;
-  } else {
-    return false;
-  }
+ if (num % 2 === 0){
+   return true;
+ }
+ else
+   return false
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num % 2 ===1) {
-    return true;
-  } else {
+  if (num % 2 === 0){
     return false;
   }
+  else
+    return true
 }
 
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  return Math.pow(num,2);
+  return num * num;
 }
 
 function elevarAlCubo(num) {
@@ -171,15 +169,14 @@ function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  var combinado = nombre + ' ' + apellido;
-  return combinado; 
+  return nombre + ' ' + apellido 
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return 'Hola' + ' ' + nombre + '!';
+  return "Hola" + ' ' + nombre + '!';
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
